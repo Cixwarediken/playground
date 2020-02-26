ab7c67ae42f8035a5f65b44d38e6286d8dd358ad
Diken Khadka <cixwarediken@gmail.com>
2020-02-26 21:25:35 +0545

    prime.php

---
+++ prime.php
@@ -0,0 +1,19 @@
+<?php 
+function primeCheck($number)
+{ 
+    if ($number == 1) 
+    return 0; 
+    for ($i = 2; $i <= $number/2; $i++)
+    { 
+        if ($number % $i == 0) 
+            return 0; 
+    } 
+    return 1; 
+} 
+$number = 31; 
+$flag = primeCheck($number); 
+if ($flag == 1) 
+    echo "The number is prime"; 
+else
+    echo "The given number is not prime"
+?>
