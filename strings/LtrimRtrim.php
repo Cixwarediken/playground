67977a233fc3bd49f6adbf6f21c79bdceb9c6ff7
Diken Khadka <cixwarediken@gmail.com>
2020-03-02 15:37:56 +0545

    ltrim and rtrim

---
+++ strings/LtrimRtrim.php
@@ -0,0 +1,8 @@
+<?php
+    echo 'This is the example of the ltrim and rtrim :'."<br>";
+    $str ="Killer Bee";
+    echo $str."<br>";
+    echo rtrim($str,"Bee");
+    echo"<br>";
+    echo ltrim($str,"Killer");
+?>
