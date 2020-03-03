be100259b4e9a063db3d98178c18428be8d5f33b
Diken Khadka <cixwarediken@gmail.com>
2020-03-03 08:46:40 +0545

    tasks for string and array

---
+++ tasks/exercitation.php
@@ -0,0 +1,10 @@
+<?php
+    $str="exercitation";
+    echo 'The given word is :'. $str."<br>";
+    echo substr($str,4,3)."<br>";
+    echo 'The position of the \'i\' is :'.strpos($str,"i")."<br>";
+    echo 'After removing exer : '.substr_replace($str,'',0,4)."<br>";
+    echo 'Removing \'tation\' with \'se\' is :'.substr_replace($str,'se',6)."<br>";
+    $str1="ex|er|ci|ta|ti|on";
+    print_r (explode('|',$str1,6));
+?>
