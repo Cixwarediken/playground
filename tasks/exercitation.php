227655c12332e9984851cf384de689191306bd29
Diken Khadka <cixwarediken@gmail.com>
2020-03-03 21:31:35 +0545

    manipulating the word using string functions

--- tasks/exercitation.php
+++ tasks/exercitation.php
@@ -5,6 +5,7 @@
     echo 'The position of the \'i\' is :'.strpos($str,"i")."<br>";
     echo 'After removing exer : '.substr_replace($str,'',0,4)."<br>";
     echo 'Removing \'tation\' with \'se\' is :'.substr_replace($str,'se',6)."<br>";
-    $str1="ex|er|ci|ta|ti|on";
-    print_r (explode('|',$str1,6));
+   // $str1="ex|er|ci|ta|ti|on";
+   // print_r (explode('|',$str1,6));
+    print_r(str_split("exercitation",2)); //spliting with 2 letters
 ?>
